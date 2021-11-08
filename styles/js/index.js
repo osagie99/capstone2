353c6786c06a8b0ab00651cd7ca2de86d47e3dbd
Osagie99 <osagieabel@icloud.com>
2021-11-08 21:53:23 +0100

    Firs Commit

---
+++ styles/js/index.js
@@ -0,0 +1,12 @@
+function myfunction() {
+    // var x = document.querySelector('.dropdown');
+    // if(x.style.display !== "block") {
+    //     x.style.display = "block"
+    // } 
+    if(window.matchMedia("max-width: 650px")) {
+        var x =  document.querySelector('.dropdown');
+        b
+    }
+    
+
+}
